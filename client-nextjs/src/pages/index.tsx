bec9d18cb6880f070537e31c85b4a3087b0b3678
Angelo Zero <angelo.akm@gmail.com>
2022-10-29 16:54:41 -0300

    creating the input component

--- client-nextjs/src/pages/index.tsx
+++ client-nextjs/src/pages/index.tsx
@@ -1,15 +1,26 @@
-import Head from "next/head"
+import logoImg from '../../public/50-white-big-shell-logo.png'
+import styles from '../../styles/home.module.scss'
 
+import Head from 'next/head'
+import Image from 'next/image'
+
+import { Input } from '../components/ui/input/input'
 
 export default function Home() {
   return (
-   <>
-   <Head>
-     <title>BigShell Pizza</title>
-   </Head>
-   <div>
-     <h1>BigShell Pizza</h1>
-   </div>
-   </>
+    <>
+      <Head>
+        <title>BigShell Pizza - Login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt={"Logo BigShell Pizza"} />
+        <div className={styles.login}>
+          <form>
+          <Input placeholder='Email' type='text'/>
+          <Input placeholder='Password' type='password'/>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
